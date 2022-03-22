c9ddeff71415c3d6c5a8a90c06e2bf386195ff71
olis-well <97517376+olis-well@users.noreply.github.com>
2022-03-22 10:54:05 +0100

    Subiendo d nuevo Queries Uni Mysql corrected

--- BaseDatosUniversidad.sql
+++ BaseDatosUniversidad.sql
@@ -5,7 +5,7 @@ WHERE tipo = 'alumno'
 ORDER BY apellido1, apellido2, nombre ASC ;
 -- 02 Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades.
 SELECT apellido1 , apellido2 , nombre , telefono, tipo 
-FROM persona,asignatura
+FROM persona
 WHERE tipo = 'alumno'
 AND telefono IS NULL;
 -- 03 Retorna el llistat dels alumnes que van néixer en 1999.
@@ -26,25 +26,28 @@ WHERE asig.cuatrimestre = 1
 AND asig.curso = 3
 AND asig.id_grado = 7; 
 -- 06 Retorna un llistat dels professors juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom.
-SELECT  persona.apellido1 , persona.apellido2 , persona.nombre , departamento.nombre as dept
-FROM persona p
+SELECT  persona.apellido1 , persona.apellido2 , persona.nombre , departamento.nombre as departamento
+FROM persona 
 left JOIN profesor
-ON p.id = profesor.id_profesor
-inner join dept
-ON profesor.id_dept = departamento.id
-WHERE p.tipo = 'profesor'
-ORDER BY p.apellido1 , p.nombre ASC; 
+ON persona.id = profesor.id_profesor
+inner join departamento
+ON profesor.id_departamento = departamento.id
+WHERE persona.tipo = 'profesor'
+ORDER BY persona.apellido1 , persona.nombre ASC; 
+
 -- 07 Retorna un llistat amb el nom de les assignatures, any d'inici i any de fi del curs escolar de l'alumne amb nif 26902806M.
-SELECT persona.nif, persona.nombre, persona.apellido1, asignatura.nombre as asignatura, curso_escolar.anyo_inicio , curso_escolar.anyo_fin 
-FROM asignatura 
+SELECT persona.nif, persona.nombre, persona.apellido1, asig.nombre as asignatura, curso_escolar.anyo_inicio , curso_escolar.anyo_fin 
+FROM asignatura AS asig
 left join alumno_se_matricula_asignatura as al
-on asignatura.id = al.id_asignatura
+on asig.id = al.id_asignatura
 left join curso_escolar 
-on curso_escolar.id = asignatura.curso
+on curso_escolar.id = asig.curso
 LEFT JOIN persona
 ON al.id_alumno = persona.id
 where persona.nif = '26902806M'
 and curso_escolar.id = 1;
+
+
 -- 08 Retorna un llistat amb el nom de tots els departaments que tenen professors que imparteixen alguna assignatura en el Grau en Enginyeria Informàtica (Pla 2015).
 SELECT departamento.nombre, grado.nombre 
 FROM departamento
@@ -68,26 +71,26 @@ AND curso_escolar.id = 5;
 -- Resolgui les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN
 
 -- 01  Retorna un llistat amb els noms de tots els professors i els departaments que tenen vinculats. El llistat també ha de mostrar aquells professors que no tenen cap departament associat. El llistat ha de retornar quatre columnes, nom del departament, primer cognom, segon cognom i nom del professor. El resultat estarà ordenat alfabèticament de menor a major pel nom del departament, cognoms i el nom.
-SELECT departamento.nombre AS dpt, persona.apellido1, persona.apellido2 , persona.nombre  
+SELECT departamento.nombre AS departamento, persona.apellido1, persona.apellido2 , persona.nombre  
 FROM persona
 LEFT JOIN profesor
 ON persona.id = profesor.id_profesor
-LEFT JOIN dpt
-ON profesor.id_dpt = departamento.id
-ORDER BY dpt.nombre,  persona.apellido1, persona.apellido2, persona.nombre ASC;
+LEFT JOIN departamento
+ON profesor.id_departamento = departamento.id
+ORDER BY departamento.nombre,  persona.apellido1, persona.apellido2, persona.nombre ASC;
 -- 02 Retorna un llistat amb els professors que no estan associats a un departament.
 SELECT departamento.nombre AS dpt, persona.apellido1, persona.apellido2 , persona.nombre  
 FROM persona
 LEFT JOIN profesor
 ON persona.id = profesor.id_profesor
-LEFT JOIN dpt
-ON profesor.id_departamento = dpt.id
-WHERE dpt.nombre IS NULL;
+LEFT JOIN departamento
+ON profesor.id_departamento = departamento.id
+WHERE departamento.nombre IS NULL;
 -- 03 Retorna un llistat amb els departaments que no tenen professors associats.
 SELECT departamento.nombre AS dpt , persona.apellido1 
 FROM departamento
 LEFT JOIN profesor
-ON  dpt.id = profesor.id_dpt
+ON  departamento.id = profesor.id_departamento
 LEFT JOIN persona
 ON  profesor.id_profesor = persona.id
 WHERE persona.apellido1 IS NULL;
@@ -97,15 +100,15 @@ FROM persona
 LEFT JOIN profesor
 ON persona.id =profesor.id_profesor  
 LEFT JOIN asignatura
-ON profesor.id_departamento = asig.id_profesor
+ON profesor.id_departamento = asignatura.id_profesor
 WHERE persona.tipo = 'profesor'
-AND asig.nombre IS NULL
+AND asignatura.nombre IS NULL
 ORDER BY persona.apellido1 ASC;
 -- 05 Retorna un llistat amb les assignatures que no tenen un professor assignat.
 SELECT  asignatura.nombre AS  asig ,  persona.apellido1 AS apellido_profesor 
 FROM asignatura
 LEFT JOIN profesor
-ON asig.id_profesor = profesor.id_departamento    
+ON asignatura.id_profesor = profesor.id_departamento    
 LEFT JOIN persona
 ON profesor.id_profesor = persona.id 
 WHERE persona.apellido1 IS NULL;
@@ -140,10 +143,10 @@ ON profesor.id_departamento = departamento.id
 WHERE persona.tipo = "profesor"
 ORDER BY persona.nombre ASC;
 -- 04 Retorna un llistat amb tots els departaments i el nombre de professors que hi ha en cadascun d'ells. Tingui en compte que poden existir departaments que no tenen professors associats. Aquests departaments també han d'aparèixer en el llistat.
-SELECT departamento.nombre AS dpt, persona.apellido1, persona.nombre  
+SELECT departamento.nombre AS departamento, persona.apellido1, persona.nombre  
 FROM departamento
 LEFT JOIN profesor
-ON dept.id = profesor.id_dpt
+ON departamento.id = profesor.id_departamento
 LEFT JOIN persona
 ON profesor.id_profesor =persona.id;
 -- 05 Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingui en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
